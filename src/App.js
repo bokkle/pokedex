19fa7e5e28259627e4748bc53c36824c4829a219
bokkle <mitch.r_@hotmail.com>
2024-02-09 21:45:10 -0500

    added search functionality and displaying poke sprites

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   const [search, setSearch] = useState('');
   const [pokeName, setPokeName] = useState('');
   const [pokeImg, setPokeImg] = useState('');
+  const [evolutionChain, setEvolutionChain] = useState('');
+
+  console.log(evolutionChain)
 
   const handleSearch = (e) => {
     setSearch(e.target.value);
@@ -15,24 +18,60 @@ function App() {
     setPokeName(search.toLowerCase());
   };
 
+  // GET POKE FORM BY NAME
   useEffect(() => {
+    if (!pokeName) return;
     const pokemon = async () => {
-      if (!pokeName) return;
       const res = await fetch(
         `https://pokeapi.co/api/v2/pokemon-form/${pokeName}/`
       );
       const data = await res.json();
       setPokeImg(data.sprites.front_default);
+      console.log(data);
     };
     pokemon();
   }, [pokeName]);
 
+  // GET EVOLUTION CHAIN URL
+  useEffect(() => {
+    if (!pokeName) return;
+
+    const getEvolutionChain = async () => {
+      const res = await fetch(
+        `https://pokeapi.co/api/v2/pokemon-species/${pokeName}/`
+      );
+      const data = await res.json();
+      console.log(data);
+      setEvolutionChain(data.evolution_chain.url)
+    };
+    getEvolutionChain();
+  }, [pokeName]);
+
+  // GET EVOLUTIONS
+  useEffect(() => {
+    if (!evolutionChain) return;
+
+    const getEvolutions = async () => {
+      const res = await fetch(evolutionChain);
+      const data = await res.json();
+      console.log(data);
+    };
+    getEvolutions()
+  }, [evolutionChain]);
+
   return (
-    <div className="container">
-      <div className="scene">
-        <img src="/pokemon.gif" alt="Pokemon scene" />
-        <Search onHandleSearch={handleSearch} onHandleSubmit={handleSubmit} />
-        <Pokemon pokeImg={pokeImg} />
+    <div className="app">
+      <div className="search-results">
+        <ul className="list">
+          <List pokeName={pokeName} pokeImg={pokeImg} search={search} />
+        </ul>
+      </div>
+      <div className="container">
+        <div className="scene">
+          <img src="/pokemon.gif" alt="Pokemon scene" />
+          <Search onHandleSearch={handleSearch} onHandleSubmit={handleSubmit} />
+          <Pokemon pokeImg={pokeImg} />
+        </div>
       </div>
     </div>
   );
@@ -57,6 +96,20 @@ const Pokemon = ({ pokeImg }) => {
     <div className="pokemon__container">
       {pokeImg && <img src={pokeImg} alt="searched pokemon" />}
     </div>
+  );
+};
+
+const List = ({ pokeName, pokeImg }) => {
+  return (
+    <>
+      <li className="list-item">
+        {pokeName.charAt(0).toUpperCase() + pokeName.slice(1)}
+        <img src={pokeImg} alt="Pokemon card" />
+      </li>
+      <li className="list-item">{pokeName}</li>
+      <li className="list-item">{pokeName}</li>
+      <li className="list-item">{pokeName}</li>
+    </>
   );
 };
 
